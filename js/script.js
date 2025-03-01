c2d52dec284a5a02263eb5f6d44894ee7378e1c8
Lucas Waunn <lucaswaunn@gmail.com>
2025-02-28 19:14:25 -0600

    Fixing Nav bar. Lots of responsive fixes

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 // JavaScript for Hamburger Menu
 document.addEventListener('DOMContentLoaded', function() {
   const hamburger = document.querySelector('.hamburger-menu');
-  const navLinks = document.querySelector('.nav-links');
+  const navMenu = document.querySelector('#nav-menu');
 
   hamburger.addEventListener('click', function() {
     // Toggle the 'active' class for the hamburger icon
     this.classList.toggle('active');
     
     // Toggle the 'show' class for the navigation menu
-    navLinks.classList.toggle('show');
+    navMenu.classList.toggle('show');
   });
   
   // Close the menu when clicking outside
   document.addEventListener('click', function(event) {
-    const isClickInside = hamburger.contains(event.target) || navLinks.contains(event.target);
+    const isClickInside = hamburger.contains(event.target) || navMenu.contains(event.target);
     
-    if (!isClickInside && navLinks.classList.contains('show')) {
+    if (!isClickInside && navMenu.classList.contains('show')) {
       hamburger.classList.remove('active');
-      navLinks.classList.remove('show');
+      navMenu.classList.remove('show');
     }
   });
 });
