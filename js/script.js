7cb4a8717fe71ab0e87e95b5cf639200462a2662
Lucas Waunn <lucaswaunn@gmail.com>
2025-03-29 22:35:24 -0500

    Finishing up home page and Charities page, added fade in, temp fix for logo, and more formatting.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const faders = document.querySelectorAll(".fade-in");
+
 // JavaScript for Hamburger Menu
 document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.querySelector(".hamburger-menu");
@@ -20,5 +22,29 @@ document.addEventListener("DOMContentLoaded", function () {
       hamburger.classList.remove("active");
       navMenu.classList.remove("show");
     }
+  });
+
+  // Fix for fade-in effect
+  const appearOptions = {
+    threshold: 0.1,
+  };
+
+  const appearOnScroll = new IntersectionObserver(function (
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+
+  faders.forEach((fader) => {
+    appearOnScroll.observe(fader);
   });
 });
