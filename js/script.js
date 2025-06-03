ac914c254dc60fd82de396d242565e6747106524
Lucas Waunn <lucaswaunn@gmail.com>
2025-06-02 20:48:11 -0500

    Fixing threshold for donations page and education page Macaila picture

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Fix for fade-in effect
   const appearOptions = {
-    threshold: 0.2,
+    threshold: 0.1,
   };
 
   const appearOnScroll = new IntersectionObserver(function (
